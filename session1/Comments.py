b5078ce10a9bacfb6493681bc7ec03d849917585
=kzanjad <=kzanjad@collegeboard.org>
2023-03-18 19:10:09 +0530

    Adding Hello Word and comments files

---
+++ session1/Comments.py
@@ -0,0 +1,4 @@
+#This is a comment
+print("Hello, World 1!")
+
+print("Hello, World 2!")  # This is a comment
---
+++ session1/HelloWord.py
@@ -0,0 +1,4 @@
+print("Hello Word");
+print("Hello Word 2")
+print("Hello Word 3")
+print("Hello Word 4")
